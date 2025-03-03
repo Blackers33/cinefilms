576a710c231957331da8054c5d01f4310fefde4c
yirui Dupuy <yirui.hao@gmail.com>
2025-03-03 10:06:11 +0100

    created connection file back-end

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -6,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('./models/connection');
 var app = express();
 
 app.use(logger('dev'));
