ca9be44e0e257f71f89ceab4a5fe233a18d0e8eb
Charlie <cleyoudec@gmail.com>
2025-03-05 10:13:41 +0100

    Navigation corrigée

--- frontend/screens/ConnexionScreen.js
+++ frontend/screens/ConnexionScreen.js
@@ -6,10 +6,32 @@ import ConnexionComponent from "../components/ConnexionComponent/ConnexionCompon
         Platform,
         View,
         ImageBackground,
+        Text
       } from "react-native";
   import { useState } from "react";
+  import Button from "../components/common/Button";
 
-  export default function ConnexionScreen() {
+  export default function ConnexionScreen({ navigation }) {
+    
+const handleConnexion = () => {
+      fetch("http://10.9.0.150:3000/users/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.result) {
+            navigation.navigate("HomeScreen", { token: data.token });
+          } else {
+            alert("Email ou mot de passe incorrect");
+          }
+        });
+    }
+
     return (
       <KeyboardAvoidingView
       
@@ -19,6 +41,11 @@ import ConnexionComponent from "../components/ConnexionComponent/ConnexionCompon
         <SafeAreaView>
           <ImageBackground source={require('../assets/wallpaper-cinefilm.jpg')} style={styles.backgroundImage}>
             <ConnexionComponent />
+            <View style={styles.buttonContainer} >
+          <Button text="Connexion" onPress={() => handleConnexion()} />
+          <Text style={styles.transitionText} >Pas encore inscrit ?</Text>
+          <Button text="Inscrivez-vous" onPress={() => navigation.navigate("Inscription")} />
+          </View>
           </ImageBackground>
         </SafeAreaView>
       </KeyboardAvoidingView>
@@ -37,6 +64,17 @@ import ConnexionComponent from "../components/ConnexionComponent/ConnexionCompon
       width: '100%',
       height: '100%',
       justifyContent: "center",
+      alignItems: "center",
+    },
+    transitionText: {
+      color: "#c94106",
+      fontSize: 20,
+      marginTop: 80,
+      textAlign: "center",
+    },
+    buttonContainer: {
+      width: 380,
+      marginTop: 10,
       alignItems: "center",
     },
   })
