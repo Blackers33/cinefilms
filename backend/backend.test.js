6d186d845f098888ba1ea3c6fc58bf7b337b560f
Sacha <sacha.alt@proton.me>
2025-03-07 11:42:12 +0100

    Working on TDDs

---
+++ backend/backend.test.js
@@ -0,0 +1,120 @@
+const request = require("supertest");
+const express = require("express");
+const router = require("./routes/films");
+const Film = require("./models/films");
+const app = require("./app");
+
+/**
+ * @author Sacha
+ * Le test envoie deux requêtes d'affillée pour liker et unliker un film.
+ * On vérifie que le film a bien été liké puis unliké par l'utilisateur.
+ */
+
+it("POST /films/:filmId/like", async () => {
+	// user et son userId correspondant.
+	const user = "2uttv9RTPa9iTljk6x4uDJOxUMSaPftq";
+	const userId = "67c83bd5468643ecb0415635";
+
+	// On envoie une requête POST pour liker le film 1
+	const resWithLike = await request(app).post("/films/1/like").send({ user });
+
+	// On envoie une requête POST pour unliker le film 1
+	// (même requête mais puisque le like de l'utilisateur est présent, il se supprimera)
+	const resWithoutLike = await request(app)
+		.post("/films/1/like")
+		.send({ user });
+
+	console.log(resWithoutLike.body);
+
+	// On vérifie que le film 1 a bien été liké par l'utilisateur
+	expect(resWithLike.statusCode).toBe(200);
+	expect(resWithLike.body.result).toBe(true);
+	expect(resWithLike.body.film.likes).toContain(userId);
+
+	// On vérifie que le film 1 a bien été unliké par l'utilisateur
+	expect(resWithoutLike.statusCode).toBe(200);
+	expect(resWithoutLike.body.result).toBe(true);
+	expect(resWithoutLike.body.film.likes).not.toContain(userId);
+});
+
+/**
+ * @author Yirui
+ */
+
+//Test pour simuler une requête PUT afin de modifier le profil de cet utilisateur.
+it(`PUT /users/profil/`, async () => {
+	//exemple token de l'utilisateur qui existe dèjà dans les bases des données
+	const token = "_ZpeuBlpvOL6Qd1yLwyg50_GhAxA-cMl";
+    
+	const res = await request(app)
+		.put(`/users/profil/${token}`)
+		.send({
+			avatar:
+				"https://image.noelshack.com/fichiers/2015/12/1426650974-quiz-les-personnages-de-tintin-5472.jpeg",
+			age: 25,
+			genre: "Homme",
+			location: {
+				name: "Paris",
+				latitude: 48.859,
+				longitude: 2.347,
+			},
+			favMovies: ["Action", "Adventure"],
+			favGenres: [447277, 812],
+			biography: "je ne suis pas un robot",
+		});
+
+	expect(res.statusCode).toBe(200);
+	expect(res.body.result).toBe(true);
+});
+
+/**
+ * @author Mathilde
+ */
+//Test que cet utilisateur existe
+it("POST /signin", async () => {
+	const res = await request(app).post("/users/signin").send({
+		email: "gringo@gmail.com",
+		password: "A",
+	});
+	console.log(res.body);
+	expect(res.statusCode).toBe(200);
+	expect(res.body.result).toBe(true);
+	expect(res.body.token).toBe("AJrdD3LbgYExWqbR81vDDCwwF3xc7Qtt");
+});
+
+/**
+ * @author Kahina
+ */
+
+const _app = express();
+_app.use(express.json());
+_app.use(router);
+
+jest.mock("./models/films");
+
+describe("GET /:filmId/film", () => {
+	it("should return a film when it exists", async () => {
+		const testFilm = { tmdbId: 123, likes: [], comments: [] };
+		Film.findOne.mockResolvedValue(testFilm); // Correction ici
+
+		const res = await request(_app).get("/123/film");
+
+		expect(res.statusCode).toBe(200);
+		expect(res.body.result).toBe(true);
+		expect(res.body.film).toEqual(testFilm);
+	});
+});
+
+/**
+ * @author Charlie
+ */
+
+it("POST /signup", async () => {
+	const res = await request(app).post("/users/signup").send({
+		username: "test",
+		password: "test",
+		email: "test@gmail.com",
+	});
+	expect(res.statusCode).toBe(200);
+	expect(res.body.result).toBe(true);
+});
