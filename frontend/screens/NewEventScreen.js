8170b30e9a09a1f09f98d5c620714bfbfcf35d1c
Mathilde <mathilde.roussille@hotmail.fr>
2025-03-10 15:17:45 +0100

    NewEventsScreen

--- frontend/screens/NewEventScreen.js
+++ frontend/screens/NewEventScreen.js
@@ -1,26 +1,24 @@
+//Création du screen pour créer un nouvel évènement
+
 import {
     StyleSheet,
-    KeyboardAvoidingView,
     SafeAreaView,
     TouchableOpacity,
     View,
     Image,
     Text,
-    ScrollView,
     TextInput,
     StatusBar,
 } from "react-native";
-import Send from 'react-native-vector-icons/Feather'
+
 
 export default function NewEventScreen({ navigation }) {
 
     return (
         <SafeAreaView style={styles.safeArea}>
-            <StatusBar  backgroundColor="white"/>
+            <StatusBar barStyle="dark-content" backgroundColor="white" />
+            <Text style={styles.title}>Créer son évènement</Text>
             <View style={styles.container}>
-                <View>
-                    <Text style={styles.onglet}>Créer un évènement</Text>
-                </View>
                 <View style={styles.infos}>
                     <View style={styles.inputBubble}>
                         <TextInput style={styles.input}
@@ -39,7 +37,7 @@ export default function NewEventScreen({ navigation }) {
                     </View>
                 </View>
                 <View>
-                    <Image></Image>
+                    <Image style={styles.image} source={require('../assets/map.jpg')} />
                 </View>
                 <View>
                     <View style={styles.inputBubble}>
@@ -48,19 +46,19 @@ export default function NewEventScreen({ navigation }) {
                             placeholderTextColor="black" ></TextInput>
                     </View>
                     <View style={styles.inputBubble}>
-                        <TextInput style={styles.input}
-                            placeholder="Description"
+                        <TextInput style={styles.inputDescription}
+                            placeholder="Description de l'évènement"
                             placeholderTextColor="black"></TextInput>
                     </View>
-                    <Text>Inviter des personnes</Text>
+                    <Text style={styles.text}>Inviter des personnes</Text>
                 </View>
-                <View>
+                <View style={styles.buttonContainer}>
                     <TouchableOpacity onPress={() => handleSubmitMessage()} style={styles.addEvent} activeOpacity={0.8}>
-                        <Send name='Créer' size={30} color={'#C94106'} />
+                        <Text style={styles.buttonText}>Créer</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity onPress={() => handleSubmitMessage()} style={styles.removeEvent} activeOpacity={0.8}>
-                        <Send name='Supprimer' size={30} color={'#C94106'} />
+                        <Text style={styles.buttonText}>Annuler</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -71,18 +69,33 @@ export default function NewEventScreen({ navigation }) {
 const styles = StyleSheet.create({
     safeArea: {
         flex: 1,
-        backgroundColor: 'white',
-        paddingTop: `Platform.OS === 'ios' ? 30 : 0,`
+        backgroundColor: 'rgb(30.28.26)',
+
     },
     container: {
         flex: 1,
         backgroundColor: 'rgb(30.28.26)',
-      
+
     },
-    onglet: {
+    title: {
         fontFamily: 'Mulish',
+        fontSize: 24,
         color: 'white',
+        textAlign: 'center',
+        marginVertical: 10,
+        marginTop: 30,
+        fontWeight: 600,
     },
+
+    image: {
+        width: 150,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 100,
+
+    },
+
     inputBubble: {
         borderWidth: 1,
         borderRadius: 20,
@@ -98,4 +111,46 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         backgroundColor: 'white',
     },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 20,
+    },
+    addEvent: {
+        backgroundColor: 'rgba(201, 65, 6, 0.78)',
+        padding: 10,
+        borderRadius: 10,
+        flex: 1,
+        marginRight: 10,
+    },
+    removeEvent: {
+        backgroundColor: 'rgb(164, 163, 163)',
+        padding: 10,
+        borderRadius: 10,
+        flex: 1,
+        marginLeft: 10,
+
+
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 18,
+        fontFamily: 'Mulish',
+    },
+
+    inputDescription: {
+        height: 80,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 20,
+        paddingHorizontal: 10,
+        backgroundColor: 'white',
+        marginTop: -20,
+        marginBottom: -20,
+        paddingTop: 10,
+    }
+
+
+
 });
