882f70cea6c9faa3cb33da5acaade64672873bdb
Sacha <sacha.alt@proton.me>
2025-03-14 09:15:57 +0100

    fixed API Key in code

--- frontend/components/common/MoviesDropdown.jsx
+++ frontend/components/common/MoviesDropdown.jsx
@@ -13,8 +13,7 @@ export const MoviesDropdown = memo(({ setMovie }) => {
 		method: "GET",
 		headers: {
 			accept: "application/json",
-			Authorization:
-				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NmY4ZDQxNjAyMzFiNjE3YTA2MTU3M2ZhODA4YzlmMCIsIm5iZiI6MTczODc0NDYzMi41ODksInN1YiI6IjY3YTMyMzM4NDRkNjg2M2I3NDhhNzJlZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2GmSmaJ7gWBY4f7F4QCE_TrHH95nnNwEhrqAxg655Q4",
+			Authorization: process.env.EXPO_PUBLIC_TMDB_API,
 		},
 	};
 	const getSuggestions = useCallback(async (q) => {
